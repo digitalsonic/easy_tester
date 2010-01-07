5646092375ad73668ddeaeecae8b7a7760cb8c50
digitalsonic <digitalsonic@yahoo.cn>
2010-01-07 15:34:32 +0800

    remove defult require of db_validator

--- lib/easy_tester/validator/validator.rb
+++ lib/easy_tester/validator/validator.rb
@@ -1,5 +1,4 @@
 require 'easy_tester/validator/attr_validator'
-require 'easy_tester/validator/db_validator'
 require 'easy_tester/validator/list_validator'
 require 'easy_tester/validator/print_validator'
 require 'easy_tester/validator/simple_validator'
