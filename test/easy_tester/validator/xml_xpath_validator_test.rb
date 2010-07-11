b89ca47719699b3496486033ab46fc261f9c3c2c
DigitalSonic <digitalsonic@yahoo.cn>
2010-07-11 17:16:07 +0800

    add some testcase for xml_xpath_validator_test

--- test/easy_tester/validator/xml_xpath_validator_test.rb
+++ test/easy_tester/validator/xml_xpath_validator_test.rb
@@ -5,8 +5,9 @@ require 'easy_tester/validator/xml_xpath_validator'
 
 class SingleXmlXpathValidatorTest < Test::Unit::TestCase
   def test_validate
-    result = "<result><is_success>T</is_success></result>"
+    result = "<results><is_success>T</is_success><result>ABC</result><result>DEF</result></results>"
     validator = EasyTester::Validator::XmlXpathValidator.new
-    validator.validate(["/result/is_success='T'", "//is_success[0]='T'"], result)
+    validator.validate(["/results/is_success='T'", "//is_success[1]='T'"], result)
+    validator.validate(["//result[1]='ABC'", "//result[2]='DEF'"], result)
   end
 end
