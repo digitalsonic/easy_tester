bef99c8608e1e69efb95294eae0f0dd7e82e1dec
digitalsonic <digitalsonic@yahoo.cn>
2010-09-06 16:08:55 +0800

    specify the full qualified TxtProvider class

--- test/easy_tester/provider/web/txt_provider_test.rb
+++ test/easy_tester/provider/web/txt_provider_test.rb
@@ -5,14 +5,12 @@ require 'digest/md5'
 require 'easy_tester/provider/web/txt_provider'
 
 class TxtProviderTest < Test::Unit::TestCase
-  include EasyTester::Provider::Web
-
   # URL;HTTP METHOD;验证器类;期望结果;"参数"
   def test_parse_detail
     time = Time.new
     sign = Digest::MD5.hexdigest 'abc'
     line = "/url;get;\"param1=abc&param2=\#{Time.new}&sign=\#{md5('abc')}\";SimpleValidator;aa;bb;cc"
-    provider = TxtProvider.new
+    provider = EasyTester::Provider::Web::TxtProvider.new
     tc = provider.parse_detail line
     assert_equal '/url', tc.url
     assert_equal 'GET', tc.method
@@ -26,13 +24,13 @@ class TxtProviderTest < Test::Unit::TestCase
   end
 
   def test_process_server_and_port
-    provider = TxtProvider.new
+    provider = EasyTester::Provider::Web::TxtProvider.new
     provider.holder = {"domain" => "abc.com", "port" => "80"}
     server, port = provider.process_server_and_port "www.\#{@holder['domain']}", "\#{@holder['port']}"
     assert_equal "www.abc.com", server
     assert_equal 80, port
 
-    provider = TxtProvider.new 'UTF-8', "#{File.dirname(__FILE__)}/holder.yml"
+    provider = EasyTester::Provider::Web::TxtProvider.new 'UTF-8', "#{File.dirname(__FILE__)}/holder.yml"
     server, port = provider.process_server_and_port "www.\#{@holder['domain']}", "\#{@holder['port']}"
     assert_equal "www.123.com", server
     assert_equal 8080, port
